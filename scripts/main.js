ef9eaf824cad8e9aed6616a373124eed2fb30f70
Luque <luque.gh@gmail.com>
2022-09-19 00:07:53 -0300

    Some canvas tests...

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ Hooks.on("init", function () {
     let onDragLeftStart = async function (wrapped, ...args) {
         wrapped(...args);
         let data = args[0].data;
-        console.log("Drag Left Start", data);
+        console.log("Drag Left Start", data.origin);
     }
 
     libWrapper.register("movement-ruler", "Token.prototype._onDragLeftStart", onDragLeftStart, "WRAPPER");
@@ -11,7 +11,8 @@ Hooks.on("init", function () {
     let onDragLeftMove = async function (wrapped, ...args) {
         wrapped(...args);
         let data = args[0].data;
-        console.log("Drag Left Move", data);
+        //console.log("Drag Left Move", data.destination);
+        console.log(canvas.grid.getSnappedPosition(data.destination.x, data.destination.y));
     }
 
     libWrapper.register("movement-ruler", "Token.prototype._onDragLeftMove", onDragLeftMove, "WRAPPER");
@@ -19,7 +20,10 @@ Hooks.on("init", function () {
     let onDragLeftDrop = async function (wrapped, ...args) {
         wrapped(...args);
         let data = args[0].data;
-        console.log("Drag Left Drop", data);
+        console.log("Drag Left Drop", data.destination);
+        let originSnapped = canvas.grid.getSnappedPosition(data.origin.x, data.origin.y);
+        let destSnapped = canvas.grid.getSnappedPosition(data.destination.x, data.destination.y);
+        console.log("Grid MeasureDistance", canvas.grid.measureDistance(originSnapped, destSnapped));
     }
 
     libWrapper.register("movement-ruler", "Token.prototype._onDragLeftDrop", onDragLeftDrop, "WRAPPER");

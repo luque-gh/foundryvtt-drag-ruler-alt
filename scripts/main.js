d4f30904c8bb4a192ec72a5f14684e6656d9ea09
Luque <luque.gh@gmail.com>
2022-09-18 23:19:03 -0300

    Using lib-wrapper to register wrappers

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,30 @@
-//Use this in your module's primary JS file to log whenever a Hook event occurs.
-Hooks.once("init", function (entityType) {
-    CONFIG.debug.hooks = true
-    if (entityType === Token) {
-        console.log("Token ", entityType.name, " selected");
+Hooks.on("init", function () {
+    //CONFIG.debug.hooks = true
+    let onDragLeftStart = async function (wrapped, ...args) {
+        wrapped(...args);
+        let data = args[0].data;
+        console.log("Drag Left Start", data);
     }
+
+    libWrapper.register("movement-ruler", "Token.prototype._onDragLeftStart", onDragLeftStart, "WRAPPER");
+
+    let onDragLeftMove = async function (wrapped, ...args) {
+        wrapped(...args);
+        let data = args[0].data;
+        console.log("Drag Left Move", data);
+    }
+
+    libWrapper.register("movement-ruler", "Token.prototype._onDragLeftMove", onDragLeftMove, "WRAPPER");
+
+    let onDragLeftDrop = async function (wrapped, ...args) {
+        wrapped(...args);
+        let data = args[0].data;
+        console.log("Drag Left Drop", data);
+    }
+
+    libWrapper.register("movement-ruler", "Token.prototype._onDragLeftDrop", onDragLeftDrop, "WRAPPER");
 });
 
-Hooks.on("ready", function () {
+Hooks.once("ready", function () {
     console.log("This code runs once core initialization is ready and game data is available.");
 });

e78de1cb6aa15c126a7c1ab01ebc767c93655f99
Luque <luque.gh@gmail.com>
2022-09-20 01:29:15 -0300

    Mouse & events sucks...

--- scripts/main.js
+++ scripts/main.js
@@ -20,13 +20,16 @@ Hooks.on("init", function () {
     let onDragLeftDrop = async function (wrapped, ...args) {
         wrapped(...args);
         let data = args[0].data;
-        console.log("Drag Left Drop", data.destination);
+        let local = data.getLocalPosition(canvas.app.stage);
+        //let mouse = canvas.app.renderer.plugins.interaction.mouse;
+        //let local = mouse.getLocalPosition(canvas.app.stage);
+        console.log("Drag Left Drop", args[0]);
         let originSnapped = canvas.grid.getSnappedPosition(data.origin.x, data.origin.y);
-        let destSnapped = canvas.grid.getSnappedPosition(data.destination.x, data.destination.y);
+        let destSnapped = canvas.grid.getSnappedPosition(local.x, local.y);
         console.log("Grid MeasureDistance", canvas.grid.measureDistance(originSnapped, destSnapped));
         //console.log(game);
-        console.log(canvas);
-        let square = await canvas.scene.createEmbeddedDocuments('Drawing', [{x: destSnapped.x, y: destSnapped.y, shape: {width: 50, height: 50, type: CONST.DRAWING_TYPES.RECTANGLE}}]);
+        //console.log(canvas);
+        let square = await canvas.scene.createEmbeddedDocuments('Drawing', [{x: originSnapped.x, y: originSnapped.y, shape: {width: 50, height: 50, type: CONST.DRAWING_TYPES.RECTANGLE}}]);
         console.log(square);
     }
 

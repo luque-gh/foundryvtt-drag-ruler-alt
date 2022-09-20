90b6dafc5d2f996b1d18f4b0f0998aabb35e73fa
Luque <luque.gh@gmail.com>
2022-09-20 16:20:15 -0300

    Kinda working, but performance is terrible and bug a lot

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 import { walkOrthogonalSquareGrid } from "./util.js";
 
-var coordMap;
+var waypointArray;
+var gridArray;
+var lastCoord;
 
 Hooks.on("init", function () {
     //CONFIG.debug.hooks = true
-    coordMap = new Map();
     let onDragLeftStart = async function (wrapped, ...args) {
         wrapped(...args);
         let data = args[0].data;
         console.log("Drag Left Start", data.origin);
+        waypointArray = [];
+        gridArray = [];
+        let origin = canvas.grid.getSnappedPosition(data.origin.x - canvas.grid.grid.w / 2, data.origin.y - canvas.grid.grid.h / 2);
+        waypointArray.push({x: origin.x, y: origin.y});
+        lastCoord = {x: origin.x, y: origin.y};
     }
 
     libWrapper.register("movement-ruler", "Token.prototype._onDragLeftStart", onDragLeftStart, "WRAPPER");
@@ -16,31 +22,14 @@ Hooks.on("init", function () {
     let onDragLeftMove = async function (wrapped, ...args) {
         wrapped(...args);
         let data = args[0].data;
-        let origin = canvas.grid.getSnappedPosition(data.origin.x - canvas.grid.grid.w / 2, data.origin.y - canvas.grid.grid.h / 2);
+        let origin = waypointArray[waypointArray.length - 1];
         let dest = canvas.grid.getSnappedPosition(data.destination.x - canvas.grid.grid.w / 2, data.destination.y - canvas.grid.grid.h / 2);
-        let distance = Math.trunc(canvas.grid.measureDistance(origin, dest));
-        let key = JSON.stringify(dest);
-        if (!coordMap.has(key)) {
-            //Insert key with temporary value
-            coordMap.set(key, '');
-            let square = await canvas.scene.createEmbeddedDocuments(
-                'Drawing', 
-                [
-                    {
-                        x: dest.x,
-                        y: dest.y,
-                        fillColor: "#FF0000",
-                        strokeWidth: 0,
-                        fillType: 1,
-                        fillAlpha: 0.4,
-                        fontSize: 20,
-                        text: distance,
-                        shape: {width: canvas.grid.grid.w, height: canvas.grid.grid.h, type: CONST.DRAWING_TYPES.RECTANGLE}
-                    }]
-                );
-            //Insert final value
-            coordMap.set(key, square[0].id);
-            console.log(walkOrthogonalSquareGrid(origin, dest));
+        if (lastCoord.x != dest.x || lastCoord.y != dest.y) {
+            lastCoord.x = dest.x;
+            lastCoord.y = dest.y;
+            let pathArray = walkOrthogonalSquareGrid(origin, dest);
+            await clearGrid();
+            await buildGrid(pathArray);
         }
     }
 
@@ -49,18 +38,14 @@ Hooks.on("init", function () {
     let onDragLeftDrop = async function (wrapped, ...args) {
         wrapped(...args);
         
-        const values = Array.from(coordMap.values());
-        console.log(values);
-        canvas.scene.deleteEmbeddedDocuments('Drawing', values);
-        coordMap = new Map();
-
         //let mouse = canvas.app.renderer.plugins.interaction.mouse;
         //let local = mouse.getLocalPosition(canvas.app.stage);
         console.log("Drag Left Drop", args[0]);
-        //let local = data.getLocalPosition(canvas.app.stage);
         //console.log(game);
         console.log(canvas.grid);
-    }
+        waypointArray = [];
+        await clearGrid();
+   }
 
     libWrapper.register("movement-ruler", "Token.prototype._onDragLeftDrop", onDragLeftDrop, "WRAPPER");
 });
@@ -68,3 +53,30 @@ Hooks.on("init", function () {
 Hooks.once("ready", function () {
     console.log("This code runs once core initialization is ready and game data is available.");
 });
+
+let clearGrid = async () => {
+    await canvas.scene.deleteEmbeddedDocuments('Drawing', gridArray);
+    gridArray = [];
+}
+
+let buildGrid = async (pathArray) => {
+    for (var i = 0; i < pathArray.length; i++) {
+        let path = pathArray[i];
+        let square = await canvas.scene.createEmbeddedDocuments(
+            'Drawing', 
+            [
+                {
+                    x: path.x,
+                    y: path.y,
+                    fillColor: "#FF0000",
+                    strokeWidth: 0,
+                    fillType: 1,
+                    fillAlpha: 0.4,
+                    fontSize: 20,
+                    text: (i + 1) * 5,
+                    shape: {width: canvas.grid.grid.w, height: canvas.grid.grid.h, type: CONST.DRAWING_TYPES.RECTANGLE}
+                }]
+            );
+            gridArray.push(square[0].id);
+    }
+}

f70f5e4b8da803c659955d8c55ee565937743ccf
Luque <luque.gh@gmail.com>
2022-09-22 22:41:33 -0300

    Fixed libwrapper module register id

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ import { onDragLeftStart, onDragLeftMove, onDragLeftDrop, onDragLeftCancel } fro
 Hooks.on("init", function () {
     //CONFIG.debug.hooks = true
     initializeSettings();
-    libWrapper.register("movement-ruler", "Token.prototype._onDragLeftStart", onDragLeftStart, "WRAPPER");
-    libWrapper.register("movement-ruler", "Token.prototype._onDragLeftMove", onDragLeftMove, "WRAPPER");
-    libWrapper.register("movement-ruler", "Token.prototype._onDragLeftDrop", onDragLeftDrop, "MIXED");
-    libWrapper.register("movement-ruler", "Token.prototype._onDragLeftCancel", onDragLeftCancel, "WRAPPER");
+    libWrapper.register("drag-ruler-alt", "Token.prototype._onDragLeftStart", onDragLeftStart, "WRAPPER");
+    libWrapper.register("drag-ruler-alt", "Token.prototype._onDragLeftMove", onDragLeftMove, "WRAPPER");
+    libWrapper.register("drag-ruler-alt", "Token.prototype._onDragLeftDrop", onDragLeftDrop, "MIXED");
+    libWrapper.register("drag-ruler-alt", "Token.prototype._onDragLeftCancel", onDragLeftCancel, "WRAPPER");
 });
 
 Hooks.once("ready", function () {
